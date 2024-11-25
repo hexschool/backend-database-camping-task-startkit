5de3a3bec73708482f18ad66083df8947fa9ebd4
AaronHsieh <piafyoyo06@gmail.com>
2024-11-25 19:03:42 +0800

    feat: 修改第六大題的答案

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -354,64 +354,69 @@ GROUP BY cp.user_id;
 -- 6. 後台報表
 -- 6-1 查詢：查詢專長為重訓的教練，並按經驗年數排序，由資深到資淺（需使用 inner join 與 order by 語法)
 -- 顯示須包含以下欄位： 教練名稱 , 經驗年數, 專長名稱
-SELECT
-  u.name AS 教練名稱,
-  c.experience_years AS 經驗年數,
-  s.name AS 專長名稱
-FROM
-  "COACH" c
-  INNER JOIN "USER" u ON c.user_id = u.id
-  INNER JOIN "COACH_LINK_SKILL" cls ON c.id = cls.coach_id
-  INNER JOIN "SKILL" s ON cls.skill_id = s.id
-WHERE
-  s.name = '重訓'
-ORDER BY
-  c.experience_years DESC;
+SELECT 
+    u.name AS 教練名稱,
+    c.experience_years AS 經驗年數,
+    s.name AS 專長名稱
+FROM 
+    "COACH" c
+INNER JOIN 
+    "USER" u ON c.user_id = u.id
+INNER JOIN 
+    "COACH_LINK_SKILL" cls ON c.id = cls.coach_id
+INNER JOIN 
+    "SKILL" s ON cls.skill_id = s.id
+WHERE 
+    s.name = '重訓'
+ORDER BY 
+    c.experience_years DESC;
 
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
-SELECT
-  s.name AS 專長名稱,
-  COUNT(DISTINCT c.id) AS coach_total
-FROM
-  "COACH" c
-  INNER JOIN "COACH_LINK_SKILL" cls ON c.id = cls.coach_id
-  INNER JOIN "SKILL" s ON cls.skill_id = s.id
-GROUP BY
-  s.name
-ORDER BY
-  coach_total DESC
+SELECT 
+    s.name AS 專長名稱,
+    COUNT(c.id) AS coach_total
+FROM 
+    "SKILL" s
+INNER JOIN 
+    "COACH_LINK_SKILL" cls ON s.id = cls.skill_id
+INNER JOIN 
+    "COACH" c ON cls.coach_id = c.id
+GROUP BY 
+    s.name
+ORDER BY 
+    coach_total DESC
 LIMIT 1;
 
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
-SELECT
-  cp.name AS 組合包方案名稱,
-  COUNT(cpur.id) AS 銷售數量
-FROM
-  "CREDIT_PURCHASE" cpur
-  INNER JOIN "CREDIT_PACKAGE" cp ON cpur.credit_package_id = cp.id
-WHERE
-  DATE_TRUNC('month', cpur.purchase_at) = '2024-11-01'
-GROUP BY
-  cp.name;
+SELECT 
+    cp.name AS 組合包方案名稱,
+    COUNT(cpr.id) AS 銷售數量
+FROM 
+    "CREDIT_PACKAGE" cp
+INNER JOIN 
+    "CREDIT_PURCHASE" cpr ON cp.id = cpr.credit_package_id
+WHERE 
+    EXTRACT(MONTH FROM cpr.purchase_at) = 11
+GROUP BY 
+    cp.name;
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
-SELECT
-  SUM(cpur.price_paid) AS 總營收
-FROM
-  "CREDIT_PURCHASE" cpur
-WHERE
-  DATE_TRUNC('month', cpur.purchase_at) = '2024-11-01';
+SELECT 
+    SUM(cpr.price_paid) AS 總營收
+FROM 
+    "CREDIT_PURCHASE" cpr
+WHERE 
+    EXTRACT(MONTH FROM cpr.purchase_at) = 11;
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
-SELECT
-  COUNT(DISTINCT cb.user_id) AS 預約會員人數
-FROM
-  "COURSE_BOOKING" cb
-WHERE
-  DATE_PART('month', cb.created_at) = 11
-  AND DATE_PART('year', cb.created_at) = 2024
-  AND cb.status IN ('即將授課', '上課中', '課程已完成');
+SELECT 
+    COUNT(DISTINCT cb.user_id) AS 預約會員人數
+FROM 
+    "COURSE_BOOKING" cb
+WHERE 
+    EXTRACT(MONTH FROM cb.created_at) = 11 
+    AND cb.status = 'active';
